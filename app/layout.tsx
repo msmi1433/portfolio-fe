ab19efed424dbcb661d133c7eb4b0c5d1b692495
Myles Smith <130503063+msmi1433@users.noreply.github.com>
2024-01-08 15:31:36 +0000

    removes unnecessary fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter, Montserrat } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import Navbar from "./components/navbar/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
