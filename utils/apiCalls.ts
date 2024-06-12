f693a84292d5694b1736c46abeea96cc4687d45f
Myles Smith <130503063+msmi1433@users.noreply.github.com>
2024-06-12 17:58:10 +0100

    migrates portfolio to new API

--- utils/apiCalls.ts
+++ utils/apiCalls.ts
@@ -1,19 +1,22 @@
 export const getProjects = async () => {
-  const res = await fetch("https://msmi-portfolio.cyclic.app/api/projects", {
-    next: { revalidate: 3600 },
-  });
+  const res = await fetch(
+    "https://portfolio-db-v6qz.onrender.com/api/projects",
+    {
+      next: { revalidate: 3600 },
+    }
+  );
   const { projects } = await res.json();
   return projects;
 };
 
 export const getSingleProject = async (projectId: number) => {
   const resProj = await fetch(
-    `https://msmi-portfolio.cyclic.app/api/projects/${projectId}`,
+    `https://portfolio-db-v6qz.onrender.com/api/projects/${projectId}`,
     { next: { revalidate: 3600 } }
   );
   const { project } = await resProj.json();
   const resStack = await fetch(
-    `https://msmi-portfolio.cyclic.app/api/projects/${projectId}/stack`,
+    `https://portfolio-db-v6qz.onrender.com/api/projects/${projectId}/stack`,
     { next: { revalidate: 3600 } }
   );
   const { stack } = await resStack.json();
